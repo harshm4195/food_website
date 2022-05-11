1138c993c4531208201d62d16127c25f76850873
harshm4195 <rakeshloya123@gmail.com>
2022-05-11 12:17:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const socket = require("socket.io");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000,()=>
+console.log("server is running")
+);
